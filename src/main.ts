cf2a943c0b27cf48d82622518914eebd66a6ce4a
Nick McIntosh <nick@nickmcintosh.net>
2018-03-23 13:59:03 +0000

    Replace store only once

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import rootReducer from './duck';
 import WorkerQueue from './WorkerQueue';
 
 let store = createStore(rootReducer);
-
+let storeReplaced = false;
 export function replaceStore(externalStore: any) {
   store = externalStore;
+  storeReplaced = true;
 }
 
 export { WorkerQueue, workerQueueMiddleware, store };
