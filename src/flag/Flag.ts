a37b862ba7e30e358ee32cc46d59a377091cae6b
Nick McIntosh <nick@nickmcintosh.net>
2018-03-24 02:08:54 +0000

    Modules name typo

--- src/flag/Flag.ts
+++ src/flag/Flag.ts
@@ -14,7 +14,7 @@ export class FlagItem implements Flag.Item {
     if (!flag.status) {
       throw new Error('Must provide flag status');
     }
-    // Caution - this needs updating if Flag.ItemStatus changes.
+    // Caution - this will need updating if Flag.ItemStatus changes.
     if (!flag.status.match(/^(WORKING|HALTED|LOCKED)$/)) {
       throw new Error('Flag status must be one of HALTED, LOCKED or WORKING');
     }
